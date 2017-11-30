dbf909a17b0cfc4196161eba45c0c9fa4238ceb8
philip.hogan <programmer5000@programmer5000.com>
2017-11-30 16:37:03 -0500

    parses "12MB"

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 (() => {
   const fs = require("fs");
+  const crypto = require("crypto");
+
+  const chunkLength = 1000;
 
   const file = process.argv[2];
-  const size = parseInt(process.argv[3]);
+  fs.writeFileSync(file, "");
+
+  const sizeArg = process.argv[3];
+  let size = parseInt(sizeArg);
+
+  const options = process.argv.slice(4);
+
+  const numList = "KMGTPE";
+  if(sizeArg.slice(-1) === "B" && numList.includes(sizeArg.slice(-2, -1)) && parseInt(sizeArg.slice(0, -2))){
+    // const base = parseInt(sizeArg.slice(0, -2));
+    const increment = sizeArg.slice(-2, -1);
+    console.log("calculaing", sizeArg.slice(0, -2), increment + "B");
+    for(let i = 0; i < numList.indexOf(increment) + 1; i++){
+      console.log("size", size, "became", size * 1024);
+      size *= 1024;
+    }
+  }
   if(!size || size <= 0 || Math.floor(size) !== size){
     return console.error(`Size ${process.argv[3]} was not a valid number greater than 0!`);
   }
 
-  const stream = fs.createWriteStream(file);
-  if(size < 1000){
+  const getBytes = num => {
+    if(options.includes("--random") || options.includes("-r")){
+      return crypto.randomBytes(num);
+    }else if(options.includes("--newline") || options.includes("-n")){
+      return "\n".repeat(num);
+    }else{
+      return "\0".repeat(num);
+    }
+  };
+
+  if(size < chunkLength){
     console.log("wrote", size, "bytes to", file);
-    stream.write("\0".repeat(size));
+    fs.appendFileSync(file, getBytes(size));
   }else{
     let left = size;
     do{
-      const bytesToWrite = Math.min(left, 1000);
+      const bytesToWrite = Math.min(left, chunkLength);
       console.log("wrote", bytesToWrite, "bytes to", file);
-      stream.write("\0".repeat(bytesToWrite));
+      fs.appendFileSync(file, getBytes(bytesToWrite));
       left -= bytesToWrite;
     }while(left > 0);
   }
-  stream.close();
 })();
